463875e88c97ed14c5e5052ffe02ae383f57cbac
Ibrahim Joseph <josephibrahi@gmail.com>
2019-04-17 01:38:58 +0100

    Comment: Remove all comment

--- app.js
+++ app.js
@@ -4,16 +4,10 @@ const db = require('./db.json');
 console.log('Welcome to my ecommerce app...');
 
 let newUser = function (username, email, password) {
-
-    // get the length of the db + 1 and use it as the next id
     let id = null;
     db.users.length ? id = db.users[db.users.length - 1].id + 1 : id = 1;
-
-    // push some data to the db
     db.users.push({id: id,username: username,email: email,password: password});
-
-    // write the db to a file
-    let json = JSON.stringify(db); // stringify the db file to json object
+    let json = JSON.stringify(db); 
     fs.writeFileSync('db.json', json, 'utf8');
 }
 
